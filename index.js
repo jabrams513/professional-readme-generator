19863ee8fbbe80000a84aa069d6f6ba80b413e03
jabrams513 <145389973+jabrams513@users.noreply.github.com>
2023-12-15 16:04:58 -0500

    create a function to write a README file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const generateMarkdown = require("./generateMarkdown");
 // TODO: Create an array of questions for user input
 
 // TODO: Create a function to write README file
+function writeToFile(fileName, data) { fs.writeFile(fileName, generateMarkdown(data) + "\n", (error => error ? console.log(error) : console.log("Success"))); }
 
 // TODO: Create a function to initialize app
 
