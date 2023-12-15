bcd2c403c80376b16ef6895b23bb774d038ad876
jabrams513 <145389973+jabrams513@users.noreply.github.com>
2023-12-15 16:12:26 -0500

    create a function that returns the license link

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -17,7 +17,14 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "None"){
+    return ""
+  }
+  else {
+    return "- [License](#license)"
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
